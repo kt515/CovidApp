380f74edc0a5ab23ed390c76ceb22a9df83d9181
kt515 <72653260+kt515@users.noreply.github.com>
2021-05-20 17:57:21 +0800

    Update UserRepository.java

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -13,4 +13,6 @@ public interface UserRepository extends JpaRepository<User,String> {
 
     @Query("SELECT u FROM User u WHERE CONCAT(u.id, ' ', u.firstName, ' ', u.lastName, ' ', u.roles) LIKE %?1%")
     List<User> search(String keyword);
+    
+    List<User> findAll();
 }
